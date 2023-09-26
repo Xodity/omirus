f7b731939144355bde0ec87d99ae11aa322b1167
qiraxyz <ariqaziz74@gmail.com>
2023-09-26 20:42:56 +0700

    make cli feat: create-app

--- cli/bin.js
+++ cli/bin.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const { exec } = require('child_process');
 
 program
   .version('1.0.0')
   .description('CLI Description')
-  .option('-o, --option', 'Option Description')
-  .arguments('<arg1> <arg2>')
-  .action((arg1, arg2) => {
-    console.log('Argumen 1:', arg1);
-    console.log('Argumen 2:', arg2);
+  .arguments('<arg1>')
+  .action((arg1) => {
+    if (arg1 === 'create-app') {
+      const repoUrl = 'https://github.com/Laramus/qira-client.git';
+      const destinationFolder = '';
+
+      exec(`git clone ${repoUrl} ${destinationFolder}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error cloning repository: ${error.message}`);
+          return;
+        }
+        console.log('Repository cloned successfully.');
+      });
+    } else {
+      console.error(`Unknown command: ${arg1}`);
+    }
   });
 
 program.parse(process.argv);
