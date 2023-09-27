d5f4b0edec8f1cc999c3e01083a07b6f1db9f3bc
qiraxyz <ariqaziz74@gmail.com>
2023-09-27 08:32:58 +0700

    remake cli-feat: create-app <folderName>

--- cli/bin.js
+++ cli/bin.js
@@ -1,26 +1,39 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const fse = require('fs-extra');
 
 program
   .version('1.0.0')
   .description('CLI Description')
-  .arguments('<arg1>')
-  .action((arg1) => {
-    if (arg1 === 'create-app') {
-      const repoUrl = 'https://github.com/Laramus/qira-client.git';
-      const destinationFolder = '';
-
-      exec(`git clone ${repoUrl} ${destinationFolder}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error cloning repository: ${error.message}`);
-          return;
-        }
-        console.log('Repository cloned successfully.');
-      });
-    } else {
-      console.error(`Unknown command: ${arg1}`);
+  .command('create-app <folderName>')
+  .action((folderName) => {
+    if (folderName === '.') {
+      folderName = process.cwd();
     }
+
+    const repoUrl = 'https://github.com/Laramus/qira-client.git';
+    const destinationPath = path.resolve(folderName);
+
+    exec(`git clone ${repoUrl} ${destinationPath}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error cloning repository: ${error.message}`);
+        return;
+      }
+      console.log(`app created successfully in path '${folderName}.'`);
+
+      const gitFolder = path.join(destinationPath, '.git');
+      const ideaFolder = path.join(destinationPath, '.idea');
+      if (fs.existsSync(gitFolder)) {
+        fse.removeSync(gitFolder);
+        fse.removeSync(ideaFolder);
+        console.log(`clearing data directory in path`);
+      }
+
+      // console.log(`your app now available in '${folderName}'.`);
+    });
   });
 
 program.parse(process.argv);
